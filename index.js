19e4b73639fdd7734e3259afbf6fbcb95195205a
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-20 18:59:26 -0400

    commit tree b01d5fe9d392a0d4b3bfbe1f1f0a6e7a6120e3b2

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Write your solution in this file!
+const employee = {
+  name: "Kristina",
+  streetAddress: "E South Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  let newObj = { ...employee };
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmp = { ...employee };
+  delete newEmp[key];
+  return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+//     const newEmp = { ...employee };
+//     newEmp[key] = value;
+
+//     return newEmp;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+//     employee[key] = value;
+
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+
+//     const newEmployee = { ...employee};
+//     delete newEmployee[key];
+
+//     return newEmployee;
+
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key];
+
+//     return employee;
+
+// }
